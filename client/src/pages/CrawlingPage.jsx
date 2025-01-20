9146b5099b7be922260e26c958c18e4cf9f069fb
1000ju <ju004100@gmail.com>
2025-01-20 16:49:27 +0900

    기초 인터페이스

--- client/src/pages/CrawlingPage.jsx
+++ client/src/pages/CrawlingPage.jsx
@@ -4,19 +4,45 @@ import axios from "axios";
 import Excel from "../components/Excel";
 
 const Crawling_page = () => {
-  const [search, setSearch] = useState("조선해양"); // 검색어를 관리하기 위한 useState 훅
+  const [search, setSearch] = useState({
+    search1: "조선해양",
+    search2: "",
+    search3: "",
+  }); // 검색어를 관리하기 위한 useState 훅
   const [data, setData] = useState([{}]); // 크롤링 결과 상태를 관리하기 위한 useState 훅
   const [loading, setLoading] = useState(false);
+  const [selectBox, setSelectBox] = useState({
+    box1: true,
+    box2: false,
+    box3: false,
+  });
+  const searchChange = (e) => {
+    const { name, value } = e.target;
+    setSearch({
+      ...search,
+      [name]: value,
+    });
+  };
+  const boxChange = (e) => {
+    const { name, checked } = e.target;
+    setSelectBox({
+      ...selectBox,
+      [name]: checked,
+    });
+  };
 
   const fetchData = async () => {
     setLoading(true);
     try {
       //search기반으로 url을 만듦
-      const encodedSearch = encodeURIComponent(search); // 검색어를 URL 인코딩
+      const encodedSearch = encodeURIComponent(search.search1); // 검색어를 URL 인코딩
       const url = `https://search.naver.com/search.naver?ssc=tab.news.all&where=news&sm=tab_jum&query=${encodedSearch}`; //naver 뉴스
 
       console.log("post요청");
-      const response = await axios.post("http://localhost:5000/crawl", { url }); // 서버에 POST 요청
+      const response = await axios.post("http://localhost:5000/crawl", {
+        url,
+        selectBox,
+      }); // 서버에 POST 요청
       setData(response.data.results); // response.data는 서버에서 반환된 데이터에 접근하는 방법
       // (응답으로 results로 보냈으니 정확하게 results까지 덧붙여줘야함. props이름 끼리는 맞춰줘야 값을 받아올 수 있음음)
       console.log(response.data.results);
@@ -36,14 +62,62 @@ const Crawling_page = () => {
       <input
         className="input_section"
         type="text"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)} // 입력 필드의 값이 변경될 때 URL 상태 업데이트
+        value={search.search1}
+        name="search1"
+        onChange={searchChange} // 입력 필드의 값이 변경될 때 URL 상태 업데이트
         onKeyDown={handleKeyPress}
         placeholder="검색어를 입력하세요"
       />
       <button className="button_section" onClick={fetchData}>
         크롤링 시작
       </button>
+
+      <div className="filtering_section">
+        <input
+          type="text"
+          value={search.search2}
+          name="search2"
+          onChange={searchChange}
+          placeholder="유사검색어"
+        />
+        <input
+          type="text"
+          value={search.search3}
+          name="search3"
+          onChange={searchChange}
+          placeholder="제외검색어"
+        />
+      </div>
+
+      <div className="checkbox_section">
+        <label>
+          네이버 뉴스
+          <input
+            type="checkbox"
+            name="box1"
+            checked={selectBox.box1}
+            onChange={boxChange}
+          />
+        </label>
+        <label>
+          네이버 Blog
+          <input
+            type="checkbox"
+            name="box2"
+            checked={selectBox.box2}
+            onChange={boxChange}
+          />
+        </label>
+        <label>
+          구글 뉴스
+          <input
+            type="checkbox"
+            name="box3"
+            checked={selectBox.box3}
+            onChange={boxChange}
+          />
+        </label>
+      </div>
 
       <div className="output_section">
         {loading ? ( // 로딩 상태에 따라 메시지 표시
--- server/index.js
+++ server/index.js
@@ -27,7 +27,9 @@ const scrollPage = async (page, duration) => {
 };
 
 app.post("/crawl", async (req, res) => {
-  const { url } = req.body; // 요청 본문에서 URL을 추출
+  const { url, selectBox } = req.body; // 요청 본문에서 URL을 추출
+
+  console.log(selectBox);
   try {
     const browser = await puppeteer.launch(); // Puppeteer로 브라우저 실행
     const page = await browser.newPage(); // 새로운 페이지 생성
@@ -79,21 +81,3 @@ const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`서버가 http://localhost:${PORT}에서 실행 중입니다.`); // 서버 실행 메시지
 });
-
-// textarea.dispatchEvent(new Event('input', { bubbles: true })); // input 이벤트 발생 (반응형 UI를 위해)
-// textarea.dispatchEvent:
-
-// 이 메서드는 특정 이벤트를 지정된 DOM 요소에 디스패치(전달)합니다. 즉, 주어진 요소에서 특정 이벤트가 발생한 것처럼 처리하게 만듭니다.
-// new Event('input', { bubbles: true }):
-
-// new Event('input'): 여기서 'input'은 발생시키고자 하는 이벤트의 종류입니다. input 이벤트는 사용자가 입력 필드(예: 텍스트 상자)에 내용을 입력할 때 발생하는 이벤트입니다.
-// { bubbles: true }: 이 옵션은 이벤트가 버블링(전파)되도록 설정합니다. 버블링이란, 이벤트가 발생한 요소에서 시작하여 그 부모 요소로 전파되는 과정을 의미합니다. 이를 통해 상위 요소에서도 이 이벤트를 감지하고 처리할 수 있게 됩니다.
-
-// 이유
-// 반응형 UI 업데이트:
-
-// 많은 자바스크립트 프레임워크와 라이브러리(예: React, Vue 등)는 입력 필드의 값이 변경될 때 해당 입력 필드에 대해 input 이벤트를 감지하여 UI를 업데이트하는 방식으로 동작합니다.
-// textarea.value = sentencs;로 값을 직접 변경하더라도, 단순히 변수의 값을 변경한 것일 뿐, UI가 변경된 것을 감지하지 못할 수 있습니다. 이때 input 이벤트를 발생시켜 UI가 반응하도록 해줍니다.
-// 상태 관리:
-
-// 예를 들어, React에서는 onChange 이벤트 핸들러가 input 이벤트를 감지하여 상태를 업데이트합니다. 이 때문에 input 이벤트를 발생시키지 않으면, 상태가 업데이트되지 않거나 UI가 변경되지 않을 수 있습니다.
